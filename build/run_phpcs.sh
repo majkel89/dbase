dc3f127e748b76be3aca8c6874d79c6b877465c4
Michał Kowalik <maf.michal@gmail.com>
2015-11-16 22:16:32 +0100

    phpcs fixes

---
+++ build/run_phpcs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$DIR/.."
+
+vendor/bin/phpcs -p \
+    --standard=style/ruleset.xml \
+    -d memory_limit=256M \
+    --ignore=tests/report \
+    src/ \
+    tests/
+
+vendor/bin/phpcbf -p \
+    --standard=style/ruleset.xml \
+    -d memory_limit=256M \
+    --ignore=tests/report \
+    src/ \
+    tests/
+
