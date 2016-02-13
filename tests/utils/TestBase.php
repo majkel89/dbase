22ee6f4765719c7cadd773ea907dba51168e4741
Michał Kowalik <maf.michal@gmail.com>
2016-02-14 00:00:02 +0100

    manually reset static variables

--- tests/utils/TestBase.php
+++ tests/utils/TestBase.php
@@ -37,6 +37,7 @@ class TestBase extends PHPUnit_Framework_TestCase {
     public function setUp() {
         parent::setUp();
         MemoFactory::setInstance(null);
+        FormatFactory::setInstance(null);
     }
 
     /**
